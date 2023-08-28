ed55cc27d3cead07c76d3733e8157e1a686b390b
Ahmadou Gueye <ahmadougueye2002@gmail.com>
2023-08-28 18:35:35 +0000

    first commit

--- portfolio/index.php
+++ portfolio/index.php
@@ -1,1 +1,45 @@
 <?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ahmadou Portfolio | Accueil</title>
+    <link rel="shortcut icon" href="CSS/img/robcorplogo.ico">
+    <link rel="stylesheet" href="CSS/template.css">
+    <link rel="stylesheet" href="CSS/style.min.css">
+    <link rel="stylesheet" href="CSS/index.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="header">
+             <div id="logo">
+                <a href="index.php"> <img src="CSS/img/robcorplogo.png" alt=""></a>
+             </div>
+             <div id="menu">
+                <ul>
+                    <li><a href="index.php">Accueil</a></li>
+                    <li><a href="projets.php">Projets</a></li>
+                </ul>
+             </div>
+             <id class="intro">
+                <div class="back">
+                Ahmadou Portfolio
+                </div>
+                <div class="putch">
+                    J'ai pour objectif de developper le site de vos reves
+                </div>
+             </id>
+    </div>
+    <div class="main">
+        <div class="partie1">
+            <div class="titre">
+                
+            </div>
+        </div>
+    </div>
+</body>
+</html>
